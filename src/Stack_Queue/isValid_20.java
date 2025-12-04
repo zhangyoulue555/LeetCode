707981a88d9c73ccc57e25f3ed1569ac1f8b43fa
Zhang Youlue <2440784496@qq.com>
2025-12-04 17:17:25 +0800

    leetcode20 括号匹配

--- src/Stack_Queue/isValid_20.java
+++ src/Stack_Queue/isValid_20.java
@@ -1,5 +1,7 @@
 package Stack_Queue;
 
+import com.sun.source.tree.NewArrayTree;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.HashMap;
@@ -12,26 +14,31 @@ import java.util.Map;
 //每个右括号都有一个对应的相同类型的左括号。
 public class isValid_20 {
 
-    class Solution {
+
         public boolean isValid(String s) {
             if (s.length() % 2 != 0) { // s 长度必须是偶数
                 return false;
             }
-            Map<Character, Character> mp = new HashMap<>() {{
-                put(')', '(');
-                put(']', '[');
-                put('}', '{');
-            }};
+
+            // 创建一个双端队列（Deque）对象，
+            // ArrayDeque：是Deque接口的常用实现类，基于动态数组实现（而非链表），没有容量限制
             Deque<Character> st = new ArrayDeque<>();
+
             for (char c : s.toCharArray()) {
-                if (!mp.containsKey(c)) { // c 是左括号
-                    st.push(c); // 入栈
-                } else if (st.isEmpty() || st.pop() != mp.get(c)) { // c 是右括号
+                if (c == '(') {
+                    st.push(')'); // 入栈对应的右括号
+                } else if (c == '[') {
+                    st.push(']');
+                } else if (c == '{') {
+                    st.push('}');
+                } else if (st.isEmpty() || st.pop() != c) { // c 是右括号
                     return false; // 没有左括号，或者左括号类型不对
                 }
             }
             return st.isEmpty(); // 所有左括号必须匹配完毕
+
+
         }
     }
 
-}
+
