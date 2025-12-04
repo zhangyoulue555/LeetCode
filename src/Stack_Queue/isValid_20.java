ea0cf95f9938dc4789460100b862159f20e691cf
Zhang Youlue <2440784496@qq.com>
2025-12-04 17:32:32 +0800

    leetcode20 括号匹配

--- src/Stack_Queue/isValid_20.java
+++ src/Stack_Queue/isValid_20.java
@@ -14,7 +14,6 @@ import java.util.Map;
 //每个右括号都有一个对应的相同类型的左括号。
 public class isValid_20 {
 
-
         public boolean isValid(String s) {
             if (s.length() % 2 != 0) { // s 长度必须是偶数
                 return false;
@@ -33,12 +32,12 @@ public class isValid_20 {
                     st.push('}');
                 } else if (st.isEmpty() || st.pop() != c) { // c 是右括号
                     return false; // 没有左括号，或者左括号类型不对
+                }else {
+                    st.pop();
                 }
             }
             return st.isEmpty(); // 所有左括号必须匹配完毕
 
-
-        }
     }
 
-
+}
